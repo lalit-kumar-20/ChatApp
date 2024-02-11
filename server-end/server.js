cc8573ce742f26135d8e74e55dde9c1485ae536f
Lalit Kumar <lalit007lodhi@gmail.com>
2024-02-11 19:33:59 +0530

    code done

--- server-end/server.js
+++ server-end/server.js
@@ -15,7 +15,20 @@ require('./Socket/socket');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+// app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "*",
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers", 
+      "*",
+    );
+    next();     
+});
 
 const port = process.env.PORT || 8000;
 
@@ -24,7 +37,7 @@ app.get('*',(req,res,next)=>{
       message:'bad request'
     })
   })
-  
+
 app.use('/api/auth', authRoutes);
 app.use('/api', conversation);
 
