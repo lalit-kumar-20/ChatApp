c11125a3d6fafefbd9fa6f700cf4dc3f278fb8e8
Lalit Kumar <lalit007lodhi@gmail.com>
2024-02-11 19:49:49 +0530

    last few commits

--- server-end/server.js
+++ server-end/server.js
@@ -15,7 +15,7 @@ require('./Socket/socket');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cors());
+ app.use(cors());
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
